7166b752238c992a582db9a6b9bee6d2cbed4a4e
segun <soshice@gmail.com>
2023-06-06 14:01:10 +0100

    First Commit

---
+++ page/src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+
+  return (
+    <>
+     <header className="App-header">     
+ 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a className="btn btn-primary"
+             data-bs-toggle="collapse"
+             href="#collapseExample"
+             role="button"
+             aria-expanded="false"
+             aria-controls="collapseExample">
+        Bootstrap button
+        </a>
+    </header>
+    </>
+  )
+}
+
+export default App
